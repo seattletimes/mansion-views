feae08463744007159ef15e4ea45be89260f9cba
Audrey Carlsen <acarlsen@seattletimes.com>
2015-12-16 11:23:53 -0800

    curse everything!

--- src/js/main.js
+++ src/js/main.js
@@ -4,40 +4,42 @@
 
 require("component-responsive-frame/child");
 
-// if (!("resize" in document.body.style)) {
-  var first = document.querySelector(".first");
-  var container = document.querySelector(".image-slider");
+var first = document.querySelector(".first");
+var container = document.querySelector(".image-slider");
 
-  var resizeable = function(e) {
-    e.preventDefault();
-    var bounds = e.target.getBoundingClientRect();
-    var x = e.clientX ? e.clientX - bounds.left : e.layerX - bounds.left;
-    first.style.width = x + "px";
-  };
+var resizeable = function(e) {
+  e.preventDefault();
+  var bounds = e.target.getBoundingClientRect();
+  var x;
+  if (e.touches) { 
+    x = e.touches[0].pageX - bounds.left;
+  } else { 
+    x = e.pageX - bounds.left;
+  }
+  // var x = e.clientX ? e.clientX - bounds.left : e.layerX - bounds.left;
+  first.style.width = x + "px";
+};
 
-  first.addEventListener("mousedown", function(e) {
-    e.preventDefault();
-    container.addEventListener("mousemove", resizeable);
-  });
-  first.addEventListener("touchstart", function(e) {
-    e.preventDefault();
-    container.addEventListener("touchmove", resizeable);
-  });
-  container.addEventListener("mouseup", function() {
-    container.removeEventListener("mousemove", resizeable);
-  });
-  container.addEventListener("touchend", function() {
-    container.removeEventListener("touchmove", resizeable);
-  });
+first.addEventListener("mousedown", function(e) {
+  e.preventDefault();
+  container.addEventListener("mousemove", resizeable);
+});
+first.addEventListener("touchstart", function(e) {
+  e.preventDefault();
+  container.addEventListener("touchmove", resizeable);
+});
+container.addEventListener("mouseup", function() {
+  container.removeEventListener("mousemove", resizeable);
+});
+container.addEventListener("touchend", function() {
+  container.removeEventListener("touchmove", resizeable);
+});
 
-  var resize = function() {
-    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width < 630) {
-      document.querySelector(".image-slider .first img").style.maxWidth = width + "px";
-      document.querySelector(".image-slider .second img").style.maxWidth = width + "px";
-    }
-  };
-  
-  resize();
-  window.addEventListener('resize', resize);
-// }
+var resize = function() {
+  var width = document.querySelector(".image-slider").getBoundingClientRect().width;
+  document.querySelector(".image-slider .first img").style.maxWidth = width + "px";
+};
+
+resize();
+window.addEventListener('resize', resize);
+
